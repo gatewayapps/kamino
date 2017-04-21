7ee351b96e72317fb13710cbbda3401ac695af16
John Murphy <John Murphy>
2017-04-21 10:59:26 -0500

    user setting go to issues list

--- background.js
+++ background.js
@@ -15,3 +15,23 @@ chrome.runtime.onInstalled.addListener((details) => {
         })
     }
 })
+
+// listen for tab change requests
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // get settings
+    chrome.storage.sync.get({
+        goToList: false
+    }, (item) => {
+        if (item.goToList) {
+            // if user setting is set, open issue list and set focus and open cloned issue in tab
+            chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+                chrome.tabs.create({ url: 'https://github.com/' + request.repo + '/issues/' + request.issueNumber, selected: false })
+                chrome.tabs.update(tabs[0].id, { url: 'https://github.com/' + request.organization + '/' + request.oldRepo + '/issues', selected: true })
+            })
+        }
+        else {
+            // if user setting is not set, open open cloned issue in new tab and set focus to that tab
+            chrome.tabs.create({ url: 'https://github.com/' + request.repo + '/issues/' + request.issueNumber, selected: true })
+        }
+    })
+})
