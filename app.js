70e351bb2456cdd81e2feabf32e99520a29aaf91
John Murphy <jmichealmurphy@gmail.com>
2018-10-21 11:58:58 -0700

    Update app.js
    Co-Authored-By: jamie29w <jamie29w@gmail.com>

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function initializeExtension() {
   // and the page is not a new issue page
   // and there is no Kamino button in the DOM, proceed
   if (urlObj.url.indexOf(`${urlObj.organization}/${urlObj.currentRepo}/compare/`) < 0 &&
-    urlObj.url.indexOf`(${urlObj.organization}/${urlObj.currentRepo}/pull/`) < 0 &&
+    urlObj.url.indexOf(`${urlObj.organization}/${urlObj.currentRepo}/pull/`) < 0 &&
       urlObj.url.indexOf(`${urlObj.organization}/${urlObj.currentRepo}/issues/new`) < 0 &&
       $('.kaminoButton').length === 0) {
 
